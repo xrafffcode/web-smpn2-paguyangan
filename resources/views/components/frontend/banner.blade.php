05c4345a09025af6f6a6b1d8291a2b8f93abddd2
nafidinara <nafidinara@gmail.com>
2023-10-06 16:55:29 +0700

    refractor(fe): change style swiper

--- resources/views/components/frontend/banner.blade.php
+++ resources/views/components/frontend/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="swiper-wrapper">
         {{ $slot }}
     </div>
-    <div class="swiper-button-prev d-none d-sm-none d-md-none d-lg-flex"></div>
-    <div class="swiper-button-next d-none d-sm-none d-md-none d-lg-flex"></div>
+    <div class="swiper-button-prev d-flex d-sm-flex d-md-flex d-lg-flex"></div>
+    <div class="swiper-button-next d-flex d-sm-flex d-md-flex d-lg-flex"></div>
     <div class="swiper-pagination"></div>
 </div>
